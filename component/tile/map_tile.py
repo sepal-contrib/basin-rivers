3749f91f95ded10b66e76d6b0f12e6e00368a19d
dfguerrerom <dfgm2006@gmail.com>
2022-02-16 14:27:55 +0000

    refactor: create a trash button to remove previously created markers

--- component/tile/map_tile.py
+++ component/tile/map_tile.py
@@ -1,16 +1,18 @@
-from traitlets import CFloat, dlink
-from ipyleaflet import LocalTileLayer
+from traitlets import CFloat, dlink, Bool
+from ipyleaflet import LocalTileLayer, WidgetControl
 
+import sepal_ui.sepalwidgets as sw
 from sepal_ui.mapping import SepalMap
-from ipyleaflet import Marker
 import ipyvuetify as v
 
+import component.scripts.utils as cu
+import component.parameter as param
 
 class MapTile(SepalMap):
     
-    lat = CFloat(1.9854,allow_none=True).tag(sync=True)
-    lon = CFloat(-76.0185,allow_none=True).tag(sync=True)
-    
+    lat = CFloat(1.985, allow_none=True).tag(sync=True)
+    lon = CFloat(-76.018, allow_none=True).tag(sync=True)
+        
     def __init__(self, model, *args, **kwargs):
         
         self.model = model
@@ -23,41 +25,76 @@ class MapTile(SepalMap):
         
         self.zoom = 10
         self.center = [self.lat, self.lon]
+        
+        self.trash_btn = v.Btn(
+            children=[
+                v.Icon(children=['fa fa-trash'], small=True)
+            ], 
+            small=True
+        ).hide()
+        trash_control = WidgetControl(
+            widget=sw.Tooltip(self.trash_btn,'Remove point', right=True), 
+            position='topleft'
+        )
+        self.add_control(trash_control)
             
         dlink((self, 'lat'),(self.model, 'lat'))
         dlink((self, 'lon'),(self.model, 'lon'))
         
         self.on_interaction(self.return_coordinates)
+        self.trash_btn.on_event('click', self.trash_event)
         
-                
+    def add_layer(self, layer):
+        """Add layer and check its name"""
+        
+        if layer.name == param.marker_name:
+            self.trash_btn.show()
+        
+        super().add_layer(layer)
+    
+    def trash_event(self, *args):
+        """restore coordinates and link lat-lon widgets"""
+        
+        self.restore_coordinates()
+        
+        # Link widgets again if automatic (not manual) is selected
+        if not self.model.manual:
+            self.model.lat_link.link()
+            self.model.lon_link.link()
+
+    def restore_coordinates(self, *args):
+        """Remove marker and restore coordinates"""
+        
+        self.remove_layers_if('type', 'marker', _metadata=True)
+        self.lat, self.lon = [round(x, 3) for x in self.center]
+        
+    
     def return_coordinates(self, **kwargs):
         
-        if not self.model.disable_coords:
+        """Synchronize the cursor coordinates in the map with lat-lon traits"""
         
-            lat, lon  = kwargs['coordinates']
-
-            self.lat = round(lat, 4)
-            self.lon = round(lon, 4)
+        if not self.model.manual:
+            
+            # Only update model coordinates when the widgets are linked
+            if self.model.lat_link.linked:
                 
+                lat, lon  = kwargs['coordinates']
+                self.lat = round(lat, 3)
+                self.lon = round(lon, 3)
+
         if kwargs.get('type') == 'click':
             
-            self.model.disable_coords = not self.model.disable_coords
+            # Stop the comunication between widgets after select the point
+            # To avoid changing the captured coordinates
+            self.model.lat_link.unlink()
+            self.model.lon_link.unlink()
             
             # Remove markdown if there is one
             self.remove_layers_if('type', 'marker', _metadata=True)
             self.remove_layers_if('type', 'square', _metadata=True)
-            
-            if self.model.disable_coords:
 
-                marker = Marker(
-                    location=kwargs.get('coordinates'), 
-                    draggable=False, 
-                )
-
-                marker.__setattr__('_metadata', {'type':'marker'})
-                marker.__setattr__('name', 'Marker')
-
-                self.add_layer(marker)
+            marker = cu.get_marker(kwargs.get('coordinates'))
+            self.add_layer(marker)
             
     def remove_layers_if(self, prop, equals_to, _metadata=False):
         """Remove layers with a given property and value
@@ -75,6 +112,7 @@ class MapTile(SepalMap):
             for layer in self.layers:
                 if hasattr(layer, prop):
                     if layer.__dict__["_trait_values"][prop] == equals_to:
+                        if layer.name == param.marker_name: self.trash_btn.hide()
                         self.remove_layer(layer)
     
     
