8403f98fb5b44201dd7cec744fcda83b9b9e1ba5
dfguerrerom <dfgm2006@gmail.com>
2022-02-22 20:21:08 +0000

    refactor: add widget to define the selection method

--- component/tile/catchment_view.py
+++ component/tile/catchment_view.py
@@ -16,6 +16,7 @@ import component.parameter as param
 from component.message import cm
 
 
+
 __all__ = ["BasinView", "Dashboard"]
 
 
@@ -52,7 +53,6 @@ class BasinView(v.Card, sw.SepalWidget):
         map_ (SepalMap): Map to display the output layers
     """
 
-    all_item = [{"text": "All catchments", "value": "all"}]
 
     def __init__(self, model, map_, dashboard, *args, **kwargs):
 
@@ -69,24 +69,48 @@ class BasinView(v.Card, sw.SepalWidget):
         self.alert = sw.Alert()
 
         self.dashboard = dashboard
+        
+        self.w_type = sw.Select(
+            label=cm.basin.type.label,
+            items=[
+                {"text":cm.basin.type.all,"value":"all"},
+                {"text":cm.basin.type.filter,"value":"filter"}
+            ],
+            v_model="all",
+        )
 
-        self.w_hybasid = v.Select(
+        self.w_hybasid = sw.Select(
             label=cm.basin.basinid.label,
-            items=self.all_item,
-            v_model=self.model.hybasin_id,
+            items=[],
+            v_model=self.model.selected_hybas,
             multiple=True,
             chips=True,
-            filled=True,
-        )
+        ).hide()
 
         self.btn = sw.Btn("Calculate statistics")
 
-        self.children = [title, desc, self.w_hybasid, self.btn, self.alert]
+        self.children = [title, desc, self.w_type, self.w_hybasid, self.btn, self.alert]
 
         self.model.observe(self.fill_catchs, "hybasin_list")
+        
         self.w_hybasid.observe(self.zoom_to_selected, "v_model")
+        self.w_type.observe(self.display_filter, "v_model")
+        
+        link((self.w_hybasid, 'v_model'), (self.model, 'selected_hybas'))
 
         self.btn.on_event("click", self.calculate_statistics)
+        
+    def display_filter(self, change):
+        """Display hybasin id filter widget"""
+        
+        if change["new"] == "filter":
+            self.w_hybasid.show()
+        
+        else:
+            # All is selected
+            self.w_hybasid.hide()
+            if self.model.data:
+                self.map_.zoom_bounds(self.model.get_bounds(self.model.data))
 
     @su.loading_button(debug=True)
     def calculate_statistics(self, widget, event, data):
@@ -150,46 +174,28 @@ class BasinView(v.Card, sw.SepalWidget):
 
         new_items = [{"text": hybasid, "value": hybasid} for hybasid in change["new"]]
 
-        self.w_hybasid.items = self.all_item + new_items
-
-    su.switch("loading", "disabled", on_widgets=["w_hybasid"])
+        self.w_hybasid.items = new_items
 
     def zoom_to_selected(self, change):
         """Highlight selection and zoom over it"""
-
+        
         self.map_.remove_layers_if("name", "Selected")
-
+        
         if not change["new"]:
             return
 
-        if change["new"][-1] == "all":
-
-            # deselect previous selected elements
-            self.w_hybasid.v_model = ["all"]
-            self.map_.zoom_bounds(self.model.get_bounds(self.model.data))
-
         else:
-            # Deselect "all catchments" option
-            self.w_hybasid.v_model = [
-                id_ for id_ in self.w_hybasid.v_model if id_ != "all"
-            ]
-
+            
             # Get bounds and zoom to the object
             selected = self.model.get_selected(change["new"], from_json=True)
             bounds = self.model.get_bounds(selected)
-
+            
             selected = GeoJSON(
                 data=selected,
                 name="Selected",
-                style={"fillOpacity": 0.1, "weight": 2, "color": "white"},
+                style={"fillOpacity": 0.1, "weight": 2, "color":"black"},
+
             )
-
-            self.map_.zoom_bounds(bounds)
-
-            # This line is useful when paiting a feature collecitonm
-            # Not useful when using geopandas
-            # outline = (
-            #     ee.Image().byte().paint(featureCollection=selected, color=1, width=2)
-            # )
-
+        
+            self.map_.zoom_bounds(bounds)            
             self.map_.add_layer(selected)
