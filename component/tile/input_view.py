c4773e6aa8c299f9c2e51c59e7df8799f08e6c59
dfguerrerom <dfgm2006@gmail.com>
2022-03-09 20:31:02 +0000

    refactor: remove range slider and import from custom widgets

--- component/tile/input_view.py
+++ component/tile/input_view.py
@@ -21,7 +21,7 @@ ee.Initialize()
 __all__ = ["InputsView"]
 
 
-class InputsView(v.Card, sw.SepalWidget):
+class InputsView(cw.Card):
     """Card to capture all the user inputs
 
     Args:
@@ -48,14 +48,7 @@ class InputsView(v.Card, sw.SepalWidget):
         title = v.CardTitle(children=["Parameters"])
         desc = v.CardText()
 
-        self.w_years = v.RangeSlider(
-            label=cm.inputs.year,
-            class_="mt-5",
-            thumb_label="always",
-            min=2000 + param.gfc_min_year,
-            max=2000 + param.gfc_max_year,
-        )
-        self.w_years.v_model = [self.w_years.min, self.w_years.max]
+        self.w_years = cw.DateSlider()
 
         self.w_thres = v.Slider(
             label=cm.inputs.thres, class_="mt-5", thumb_label="always", v_model=30
@@ -73,7 +66,7 @@ class InputsView(v.Card, sw.SepalWidget):
 
         self.w_coords = cw.CoordinatesView(model=model, map_=map_)
 
-        self.btn = sw.Btn("Get upstream catchments")
+        self.btn = sw.Btn("Get upstream catchments", small=True)
 
         self.children = [
             title,
@@ -126,11 +119,5 @@ class InputsView(v.Card, sw.SepalWidget):
 
         # Get bounds and zoom to the object
         self.map_.zoom_bounds(self.model.get_bounds(self.model.data))
-
         self.map_.addLayer(forest_change, {}, "Forest change")
-
-        # outline = (
-        #     ee.Image().byte().paint(featureCollection=upstream_catch, color=1, width=2)
-        # )
-
         self.map_.add_layer(upstream_catch_gj)
