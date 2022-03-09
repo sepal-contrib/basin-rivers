c840f858d3561be8a1d2da80ac441ff6115a32e1
dfguerrerom <dfgm2006@gmail.com>
2022-03-09 20:25:02 +0000

    parametrize group names + translation dictionary + max number of cathcs in graphics

--- component/parameter/app.py
+++ component/parameter/app.py
@@ -10,12 +10,19 @@ year_range = list(range(0, gfc_max_year + 1))
 
 gfc_classes = year_range + [30, 40, 50, 51]
 
-gfc_labels = [f"loss {y+2000}" for y in year_range] + [
-    "non forest",
+gfc_labels = [f"loss_{y+2000}" for y in year_range] + [
+    "non_forest",
     "forest",
-    "gains",
-    "gain+loss",
+    "gain",
+    "gain_loss",
 ]
+
+
+# Create a group for each category and a translation dictionary
+gfc_groups = ["loss"]*(gfc_max_year+1) + gfc_labels[gfc_max_year+1:]
+gfc_translation = dict(
+    zip(gfc_classes, gfc_groups)
+)
 
 year_items = [{"text": str(y + 2000), "value": y} for y in year_range]
 
@@ -33,3 +40,8 @@ marker_name = "Marker"
 
 # Catchment properties to display
 display_prop = ["HYBAS_ID"]
+
+
+# Define a maximum number of catchments to display on the detailed graphs
+MAX_CATCH_NUMBER = 10
+
