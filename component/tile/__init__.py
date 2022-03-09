bd34e75b29297df7d8735404b0e404aed72366d0
dfguerrerom <dfgm2006@gmail.com>
2022-03-09 20:28:19 +0000

    refactor: change file name

--- component/tile/__init__.py
+++ component/tile/__init__.py
@@ -1,3 +1,6 @@
+from .basin_view import *
+from .dashboard_tile import *
+from .dashboard_view import *
 from .input_view import *
+from .inputs_tile import *
 from .map_tile import *
-from .catchment_view import *
