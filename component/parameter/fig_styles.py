7933cc931bab84099a2755fc38ac14b7d1d57c05
dfguerrerom <dfgm2006@gmail.com>
2022-03-09 20:19:48 +0000

    feat: parametrize figure/graph styles

--- component/parameter/fig_styles.py
+++ component/parameter/fig_styles.py
@@ -1,34 +1,33 @@
-
-pie_layout =dict(
-    title=dict(
+title = dict(
         font_size=20,
         x=0.5
-    ),
+)
+
+pie_layout =dict(
+    title=title
+)
+
+legend_overall_pie = dict(
     legend=dict(
         borderwidth=1,
         itemclick="toggleothers",
         orientation="h",
         yanchor="bottom",
-        y=-0.2,
+        y=-0.4,
         xanchor="right",
         x=1
     )
 )
 
-
-bar_layout =dict(
-    title=dict(
-        font_size=20,
-        x=0.5
-    ),
+legend_detailed_pie =dict(
     legend=dict(
         borderwidth=1,
         itemclick="toggleothers",
-        orientation="h",
-        yanchor="bottom",
-        y=-0.2,
-        xanchor="right",
-        x=1
+        orientation="v",
+        yanchor="top",
+        y=1,
+        xanchor="left",
+        x=0.95
     )
 )
 
@@ -36,11 +35,28 @@ pie_trace_options = dict(
     textposition='auto', 
     textinfo='percent', 
     hoverinfo='label+percent+text+value',
-    hole=.4,
+    hole=.5,
 )
 
+bar_layout =dict(
+    title=title,
+    showlegend=False,
+    uniformtext_minsize=8, 
+    uniformtext_mode='hide',
+)
 
 bar_trace_options = dict(
-    textposition='auto', 
+    textposition='outside', 
     hoverinfo='x+y+text+name',
 )
+
+trend_trace_options = dict(
+    line_shape="spline",
+    mode="lines+markers",
+)
+trend_layout=dict(
+    showlegend=False,
+    title=title,
+    hovermode="x unified",
+)
+trend_legend=dict()
